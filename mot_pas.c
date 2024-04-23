8556bfae9ca26bef91b96a2f259947e8a2c81a37
kilpinval <kilpinval@gmail.com>
2024-04-22 23:49:09 -0500

    ind_4,0_comm

---
+++ mot_pas.c
@@ -0,0 +1,14 @@
+include <conio.h>
+include <f8877A>
+
+industria 4.0
+  cual es el motor como funciona un pls
+solucuiones de industria 4.0
+  tiene que ver con arduino 
+plc arduinno nueva version,sirve para iot normas internaacconales
+civer, haace falta tener componentes fisico, gemelos digital es la copia de una maquin acomo funciona una planta un sistema elegimos python pa r haec solucione 
+industria 4.0, mas arriba de la intercomunicacion para diferentes pmarcas de plc con alguna lineas de codigo plc d fenix contag comunica rprotocolos 
+industria 4.0 vemos que cosas nos puede mejorar nuestro sistema de control facil de usar sin significar ser un expperto que sean capaces de enternder como funcion esto por dentro 
+tecnicos den roboti haciendo uinterface vision computacionañ dear los ojos a una maquina
+cientificos de datos industriales grandes volumenes de informacoin arquitectp it ayudar el flukjo de datos ingeniero de aprndiazaje automatico 
+

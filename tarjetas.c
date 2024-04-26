c3c2214bbc4ba29e159d7876eae8cc70bbbad3ff
kilpinval <kilpinval@gmail.com>
2024-04-25 21:56:14 -0500

    Create tarjetas.c

---
+++ tarjetas.c
@@ -0,0 +1,12 @@
+radio lora antena ipex
+conedtividad lora prototipado local
+master class 
+p
+pruebas de concepto (mvp) producto minimo viable para validar para mandar a produccion de placas de circuitos impresos
+desarrollar plc el firmvar es otra especializacion 
+normas ipc 
+sistemas envevidos tips de diseño 
+quisito ansi, c mension a la cordinanador mensaje cyber week mmj 50% de descuento formamos especialistas
+circuitos secuenciales sp2 en, arm serie de capact
+amazon wep server ivp 
+
